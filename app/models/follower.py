05e59dabba76c2ab72d5dfb97e0ebc33c24eff70
jack <jzhang319@gmail.com>
2023-02-14 23:05:29 -0500

    updated model, took out relationship from folowers to users

--- app/models/follower.py
+++ app/models/follower.py
@@ -9,11 +9,8 @@ class Follower(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    following_id = db.Column(db.Integer, db.ForeignKey(
-        add_prefix_for_prod("users.id")), nullable=False)
-    follower_id == db.Column(db.Integer, db.ForeignKey(
-        add_prefix_for_prod("users.id")), nullable=False)
-
+    following_id = db.Column(db.Integer, nullable=False)
+    followed_id = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=func.now())
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=func.now(), onupdate=func.now())
@@ -22,7 +19,7 @@ class Follower(db.Model):
         return {
             'id': self.id,
             'following_id': self.following_id,
-            'follower_id': self.follower_id,
+            'followed_id': self.follower_id,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
