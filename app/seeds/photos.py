e94f1dafb912e651ec37c4fb4793121fbce90f2b
jzhang319 <jzhang319@gmail.com>
2024-05-02 15:14:50 -0400

    update all seed images

--- app/seeds/photos.py
+++ app/seeds/photos.py
@@ -4,61 +4,61 @@ from app.models import db, User, Photo, environment, SCHEMA
 # Adds a demo user, you can add other users here if you want
 def seed_photos():
     demo = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075453154847629372/How-to-Choose-a-Laptop-Gear-GettyImages-1235728903.png', caption='I cant believe I got this laptop!', user_id=1
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235667338104279151/Z.png?ex=66353462&is=6633e2e2&hm=2a1d60fa2cbe9de8f936105a47bb48137e136f2f3cae9d7c73279432b35adbf3&', caption='I cant believe I got this laptop!', user_id=1
     )
     demo1 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075453638597693480/mountains-7ddde89.png', caption='This is a nice view!', user_id=1
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235667694041301042/2Q.png?ex=663534b7&is=6633e337&hm=1f3a2eab7ffbc778c61117273d36ce74ffd29923c69b9cc24b48efd3c53fec3a&', caption='This is a nice view!', user_id=1
     )
     demo2 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075453917929934869/THE-10-BEST-PARKS-IN-BROOKLYN.png', caption='I would love to swim in this', user_id=2
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235667978700197948/images.png?ex=663534fb&is=6633e37b&hm=307e0f4927fe4721999a73afe2fd5e95c8a667f7916b344aa3420c0cc3c4415b&', caption='I would love to swim in this', user_id=2
     )
     demo3 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075454258415149147/GettyImages-1235814282.png', caption='How can they actually built this?!', user_id=2
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235668370930667641/Z.png?ex=66353559&is=6633e3d9&hm=5d0613e14d959bd65bf23a44abdc8ef80646699f7a8ad626a8fbda7d32341bda&', caption='How can they actually built this?!', user_id=2
     )
     demo4 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075454532995260476/photo-1429041966141-44d228a42775.png', caption='This is magnificent', user_id=3
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235668655497285632/9k.png?ex=6635359d&is=6633e41d&hm=4036712d58f22430ad55e7d15eda15965f23ae386a12c0ba08debc424a5d55a1&', caption='This is magnificent', user_id=3
     )
     demo5 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075454803150372864/CES-2023-PEUGEOT_INCEPTION_CONCEPT_2301CN202.png', caption='Would love to drive this one day', user_id=3
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235668828810379375/2Q.png?ex=663535c6&is=6633e446&hm=c56c8f8a688652631275e5c8812ce3a3dc2b45b6041f3457ae101933ef806179&', caption='Would love to drive this one day', user_id=3
     )
     demo6 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075455109389099090/1661888151-DAL500017.png', caption='Nice car!!!', user_id=4
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669007982526484/2Q.png?ex=663535f1&is=6633e471&hm=f429ffb2b16ad3dd5bb4559114c89bf8b4b2618317ab96bdca317e4b3568d05c&', caption='Nice car!!!', user_id=4
     )
     demo7 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075455330135330816/960x0.png', caption=f"Everyone's dream", user_id=5
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669144033169478/images.png?ex=66353611&is=6633e491&hm=ef808e09f772bc26c3a8fa478dd38589ac5b4db9042687fab54857382f67e5fb&', caption=f"Everyone's dream", user_id=5
     )
     demo8 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075455689280991242/maxresdefault.png', caption='A flying car', user_id=5
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669291219554497/Z.png?ex=66353634&is=6633e4b4&hm=14b1e96ca2cb99da664909fd25ca93a3e7118fc3c531b95886e4ff5b21d49170&', caption='A flying car', user_id=5
     )
     demo9 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075455992915046430/201231114619-01-oz-ourdomain-student-housing-super-169.png', caption='I live there', user_id=6
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669430361395261/9k.png?ex=66353655&is=6633e4d5&hm=d47e33d354447d8ecd4e9384780d1044b57699e71f697f92aeb56dd20dda908b&', caption='I live there', user_id=6
     )
     demo10 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075456456725385216/maxresdefault.png', caption='tallest buildings', user_id=7
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669532849213481/9k.png?ex=6635366e&is=6633e4ee&hm=f3b6cdaf14b4419e8da4f32734ca5b3d8a2451be4d023030a3a91d2b9e24db52&', user_id=7
     )
     demo11 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075456781590986832/shutterstock_783317635-e1673245880444-1520x855.png', caption="nice tree", user_id=8
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669692161720401/9k.png?ex=66353694&is=6633e514&hm=55c5f411e27a4dc9a378bba14be5916253e16be432ec5dc7d550a0fe94430a3f&', caption="nice tree", user_id=8
     )
     demo12 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075456979226591383/21a7af1782ac440dbf1c0c93b1e21b0b.png', caption='Sky view', user_id=8
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669846406991963/2Q.png?ex=663536b9&is=6633e539&hm=6a436255610990f2c25c76db92708170bb6aa30e8d7a60ab49606e3312a81098&', caption='Sky view', user_id=8
     )
     demo13 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075457164241551441/8C8856378-130905-burj-khalifa-9a.png', caption='Working here would be nice', user_id=9
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670002812583977/2Q.png?ex=663536de&is=6633e55e&hm=ee27b02c79f50faf02742af2f4da822a6f5f04a84968eb7aad0ba2d214759baa&', caption='Working here would be nice', user_id=9
     )
     demo14 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075457686885380176/airplane-cartoon-1301493.png', caption='nice plane', user_id=9
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670264688283799/9k.png?ex=6635371c&is=6633e59c&hm=9599547aa70f26cc756c26a585314a40dbec4ccc6687864464e8c6eb5a8492a7&', caption='nice plane', user_id=9
     )
     demo15 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075457987201736734/1000px-2021-weekend-cruises-norwegian-sky_2_0.png', caption='love vacations', user_id=10
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670387606683789/9k.png?ex=6635373a&is=6633e5ba&hm=a96097615aa68c2916f88de03d32363074a8fb95730d11a257805fadb1bb31a0&', caption='love vacations', user_id=10
     )
     demo16 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075458126427463710/3-142937e1bf7198.png', caption='my new toy', user_id=10
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670487716069499/9k.png?ex=66353751&is=6633e5d1&hm=b601e0a629efaeb4c2548f1bd912a465922d260ac525358d8d893bec0203e4a3&', caption='my new toy', user_id=10
     )
     demo17 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075458259735027722/20220310_082441-scaled.png', caption='dream cruises', user_id=11
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670602833203261/9k.png?ex=6635376d&is=6633e5ed&hm=d37c322b56468ddbe1de0b82064ff22cac495b9043db731f534acdba4dac7cee&', caption='dream cruises', user_id=11
     )
     demo18 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1075458406741192746/RCI_WN_032022_CC_JGraham_Day_Aer.png', caption='best time ever', user_id=11
+        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670723339751576/9k.png?ex=6635378a&is=6633e60a&hm=6171176d87b0e4200c1f7704f4237bd5382b1186ea4a8179601ae39e00a830b0&', caption='best time ever', user_id=11
     )
 
     db.session.add(demo)
