106a56681bb9c729c4f57e410afdc54dd9d8982c
jzhang319 <jzhang319@gmail.com>
2024-05-24 15:31:19 -0400

    added all images into static folder, no need to replace

--- app/seeds/photos.py
+++ app/seeds/photos.py
@@ -4,61 +4,61 @@ from sqlalchemy import text
 # Adds a demo user, you can add other users here if you want
 def seed_photos():
     demo = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235667338104279151/Z.png?ex=66353462&is=6633e2e2&hm=2a1d60fa2cbe9de8f936105a47bb48137e136f2f3cae9d7c73279432b35adbf3&', caption='I cant believe I got this laptop!', user_id=1
+        url='/static/photos/photo1.png', caption='I cant believe I got this laptop!', user_id=1
     )
     demo1 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235667694041301042/2Q.png?ex=663534b7&is=6633e337&hm=1f3a2eab7ffbc778c61117273d36ce74ffd29923c69b9cc24b48efd3c53fec3a&', caption='This is a nice view!', user_id=1
+        url='/static/photos/photo2.png', caption='This is a nice view!', user_id=1
     )
     demo2 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235667978700197948/images.png?ex=663534fb&is=6633e37b&hm=307e0f4927fe4721999a73afe2fd5e95c8a667f7916b344aa3420c0cc3c4415b&', caption='I would love to swim in this', user_id=2
+        url='/static/photos/photo3.png', caption='I would love to swim in this', user_id=2
     )
     demo3 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235668370930667641/Z.png?ex=66353559&is=6633e3d9&hm=5d0613e14d959bd65bf23a44abdc8ef80646699f7a8ad626a8fbda7d32341bda&', caption='How can they actually built this?!', user_id=2
+        url='/static/photos/photo4.png', caption='How can they actually built this?!', user_id=2
     )
     demo4 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235668655497285632/9k.png?ex=6635359d&is=6633e41d&hm=4036712d58f22430ad55e7d15eda15965f23ae386a12c0ba08debc424a5d55a1&', caption='This is magnificent', user_id=3
+        url='/static/photos/photo5.png', caption='This is magnificent', user_id=3
     )
     demo5 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235668828810379375/2Q.png?ex=663535c6&is=6633e446&hm=c56c8f8a688652631275e5c8812ce3a3dc2b45b6041f3457ae101933ef806179&', caption='Would love to drive this one day', user_id=3
+        url='/static/photos/photo6.png', caption='Would love to drive this one day', user_id=3
     )
     demo6 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669007982526484/2Q.png?ex=663535f1&is=6633e471&hm=f429ffb2b16ad3dd5bb4559114c89bf8b4b2618317ab96bdca317e4b3568d05c&', caption='Nice car!!!', user_id=4
+        url='/static/photos/photo7.png', caption='Nice car!!!', user_id=4
     )
     demo7 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669144033169478/images.png?ex=66353611&is=6633e491&hm=ef808e09f772bc26c3a8fa478dd38589ac5b4db9042687fab54857382f67e5fb&', caption=f"Everyone's dream", user_id=5
+        url='/photos/photo8.png', caption=f"Everyone's dream", user_id=5
     )
     demo8 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669291219554497/Z.png?ex=66353634&is=6633e4b4&hm=14b1e96ca2cb99da664909fd25ca93a3e7118fc3c531b95886e4ff5b21d49170&', caption='A flying car', user_id=5
+        url='/static/photos/photo9.png', caption='A flying car', user_id=5
     )
     demo9 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669430361395261/9k.png?ex=66353655&is=6633e4d5&hm=d47e33d354447d8ecd4e9384780d1044b57699e71f697f92aeb56dd20dda908b&', caption='I live there', user_id=6
+        url='/static/photos/photo10.png', caption='I live there', user_id=6
     )
     demo10 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669532849213481/9k.png?ex=6635366e&is=6633e4ee&hm=f3b6cdaf14b4419e8da4f32734ca5b3d8a2451be4d023030a3a91d2b9e24db52&', caption="I cannot wait" ,user_id=7
+        url='/static/photos/photo11.png', caption="I cannot wait" ,user_id=7
     )
     demo11 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669692161720401/9k.png?ex=66353694&is=6633e514&hm=55c5f411e27a4dc9a378bba14be5916253e16be432ec5dc7d550a0fe94430a3f&', caption="nice tree", user_id=8
+        url='/static/photos/photo12.png', caption="nice tree", user_id=8
     )
     demo12 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235669846406991963/2Q.png?ex=663536b9&is=6633e539&hm=6a436255610990f2c25c76db92708170bb6aa30e8d7a60ab49606e3312a81098&', caption='Sky view', user_id=8
+        url='/static/photos/photo13.png', caption='Sky view', user_id=8
     )
     demo13 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670002812583977/2Q.png?ex=663536de&is=6633e55e&hm=ee27b02c79f50faf02742af2f4da822a6f5f04a84968eb7aad0ba2d214759baa&', caption='Working here would be nice', user_id=9
+        url='/static/photos/photo14.png', caption='Working here would be nice', user_id=9
     )
     demo14 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670264688283799/9k.png?ex=6635371c&is=6633e59c&hm=9599547aa70f26cc756c26a585314a40dbec4ccc6687864464e8c6eb5a8492a7&', caption='nice plane', user_id=9
+        url='/static/photos/photo15.png', caption='nice plane', user_id=9
     )
     demo15 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670387606683789/9k.png?ex=6635373a&is=6633e5ba&hm=a96097615aa68c2916f88de03d32363074a8fb95730d11a257805fadb1bb31a0&', caption='love vacations', user_id=10
+        url='/static/photos/photo16.png', caption='love vacations', user_id=10
     )
     demo16 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670487716069499/9k.png?ex=66353751&is=6633e5d1&hm=b601e0a629efaeb4c2548f1bd912a465922d260ac525358d8d893bec0203e4a3&', caption='my new toy', user_id=10
+        url='/static/photos/photo17.png', caption='my new toy', user_id=10
     )
     demo17 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670602833203261/9k.png?ex=6635376d&is=6633e5ed&hm=d37c322b56468ddbe1de0b82064ff22cac495b9043db731f534acdba4dac7cee&', caption='dream cruises', user_id=11
+        url='/static/photos/photo18.png', caption='dream cruises', user_id=11
     )
     demo18 = Photo(
-        url='https://cdn.discordapp.com/attachments/885032629299212308/1235670723339751576/9k.png?ex=6635378a&is=6633e60a&hm=6171176d87b0e4200c1f7704f4237bd5382b1186ea4a8179601ae39e00a830b0&', caption='best time ever', user_id=11
+        url='/static/photos/photo19.png', caption='best time ever', user_id=11
     )
 
     db.session.add(demo)
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,37 +5,37 @@ from sqlalchemy import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235671445661814844/E3iyNtMQ1ksOp1N7mEBY0USRiJW2kvuv3eajzY7sIIgSLHdjBe9frdRbuSckhkIf8Hv1E2luXy6qsAAAAASUVORK5CYII.png?ex=66353836&is=6633e6b6&hm=b4d86434cb61effd49f58afc201be373d38d9966264bb425df65d580308c03ea&'
+        username='Demo', email='demo@aa.io', password='password', profile_url='/static/profile_images/profile_image-1.jpeg'
         )
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235671483582382191/images.png?ex=6635383f&is=6633e6bf&hm=30c10b8a208db5e165566dab835e13ad1410dc082313e5cae1892cdfeb31e007&'
+        username='marnie', email='marnie@aa.io', password='password', profile_url='/static/profile_images/profile_image-2.png'
         )
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235671514309722113/images.png?ex=66353846&is=6633e6c6&hm=1c52b6cd93847a5a8cf3775506cb30cbc8f39685ed2e9a3998980aa04013b931&'
+        username='bobbie', email='bobbie@aa.io', password='password', profile_url='/static/profile_images/profile_image-3.jpeg'
         )
     dave = User(
-        username='dave', email='dave@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235671582681075823/images.png?ex=66353856&is=6633e6d6&hm=345584951bd6b9353f6e1f8289cfeeeb1a62569ff7ff315560e79aa56763171b&'
+        username='dave', email='dave@aa.io', password='password', profile_url='/static/profile_images/profile_image-4.png'
         )
     stan = User(
-        username='stan', email='stan@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689341074931843/WGgdHXC6NCL1YbW2llpMfbapjLXhCmhSha7Z20C2Sim0frbaPJ60QcQhTodmDEjB6Z6JbRNLoBOKZPEpcQxWqrsV3fQWc73dB9iv5X0HnQZn270TJdmXjEKezEamuz0d7equ80ml0Oun902zu1Le2243NWGmdD93TAgTv6ArzAAAAABJRU5ErkJggg.png?ex=663548e0&is=6633f760&hm=a852e5a5eb7fd553ca07319ed017400a7e2dcd61446f6b025f5ea5fda655b2c3&'
+        username='stan', email='stan@aa.io', password='password', profile_url='/static/profile_images/profile_image-5.png'
     )
     winston = User(
-        username='winston', email='winston@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689445576020039/images.png?ex=663548f9&is=6633f779&hm=629f2736754d2503e548b9ce29b8be47b1217edf90e8c9d3806629e536d7c3e4&'
+        username='winston', email='winston@aa.io', password='password', profile_url='/static/profile_images/profile_image-6.png'
     )
     jess = User(
-        username='jess', email='jess@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689524244381786/images.png?ex=6635490c&is=6633f78c&hm=534c3689054dc24b8a6a60913889a79f666ee37604bf682c69c1d20a11ccc8f3&'
+        username='jess', email='jess@aa.io', password='password', profile_url='/static/profile_images/profile_image-7.png'
     )
     candie = User(
-        username='candie', email='candie@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689628057604096/images.png?ex=66354925&is=6633f7a5&hm=6a750e5d8eab8e9adf4a617ce21063eba4ba68129a007844570dc2f1fc33979f&'
+        username='candie', email='candie@aa.io', password='password', profile_url='/static/profile_images/profile_image-8.png'
     )
     mario = User(
-        username='mario', email='mario@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689748111167560/9k.png?ex=66354941&is=6633f7c1&hm=c5c64f7fec989101a0c59dfd03697c95f2b1b77d784d4eb9ec6f5bc70520ec53&'
+        username='mario', email='mario@aa.io', password='password', profile_url='/static/profile_images/profile_image-9.png'
     )
     liugi = User(
-        username='liugi', email='liugi@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689878541434970/2Q.png?ex=66354961&is=6633f7e1&hm=2e4e2e5cde608d1d173e3a73d52c5c86ba7fa8adfa5d810fef76549b9b0dda04&'
+        username='liugi', email='liugi@aa.io', password='password', profile_url='/static/profile_images/profile_image-10.png'
     )
     bowser = User(
-        username='bowser', email='bowser@aa.io', password='password', profile_url='https://cdn.discordapp.com/attachments/885032629299212308/1235689965808128062/2Q.png?ex=66354975&is=6633f7f5&hm=2b8687c5e459bfd1c6943537eddc079c4ba3069c4406b8ee1da4378ae1208d57&'
+        username='bowser', email='bowser@aa.io', password='password', profile_url='/static/profile_images/profile_image-4.png'
     )
 
     db.session.add(demo)
